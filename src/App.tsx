6909e5207e8658b310cf35125887bc076d972c0e
phhcharlotte <phamhuyhoang020901@gmail.com>
2024-04-19 21:04:17 +0700

    Update 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
-import { useState } from "react";
-
+import type { FormProps } from "antd";
+import { Button, Form, Input } from "antd";
 import "./App.css";
-import Count from "./components/Count";
+type FieldType = {
+  username?: string;
+  password?: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    console.log("Success:", values);
+  };
 
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <>
-      <h1>test MR</h1>
-      <Count count={count} increment={() => setCount((c) => c + 1)} />
+      <div>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off">
+          <Form.Item<FieldType>
+            label="Username"
+            name="username"
+            rules={[
+              { required: true, message: "Please input your username!" },
+            ]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[
+              { required: true, message: "Please input your password!" },
+            ]}>
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </>
   );
 }
---
+++ src/constants/menu.ts
@@ -0,0 +1,20 @@
+export const MenuStudent = [
+  {
+    key: "1",
+    label: "Danh sách khoá luận",
+  },
+  {
+    key: "2",
+    label: "Báo cáo khoá luận",
+  },
+];
+export const MenuTeacher = [
+  {
+    key: "1",
+    label: "Đăng đề tài ",
+  },
+  {
+    key: "2",
+    label: "",
+  },
+];
